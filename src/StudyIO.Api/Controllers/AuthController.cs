474018dfc3b67fc91ae2eaff25df67814277bc58
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-04 18:39:31 -0300

    Finalizando a authorização com jwt retornando um objeto complexo

--- src/StudyIO.Api/Controllers/AuthController.cs
+++ src/StudyIO.Api/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using StudyIO.Api.ViewModels;
 using StudyIO.Business.Interfaces;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -74,7 +75,7 @@ namespace StudyIO.Api.Controllers
             return CustomResponse(loginUser);
         }
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -107,7 +108,20 @@ namespace StudyIO.Api.Controllers
             });
 
             var encodedToken = tokenHandler.WriteToken(token);
-            return encodedToken;
+
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
